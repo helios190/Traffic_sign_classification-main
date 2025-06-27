943503cda01af89ce39d28c96140c9be2148755e
helios190 <bintang.restu.bawono@mail.ugm.ac.id>
2025-06-27 19:51:20 +0700

    update path

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,7 +1,7 @@
-from src import model
+from src import traffic
 import numpy as np
 
 def test_forward_dummy():
     dummy = np.random.rand(1, 30, 30, 3).astype("float32")
-    mdl = model.load()
+    mdl = traffic.load()
     assert mdl.predict(dummy).shape == (1, 43)
